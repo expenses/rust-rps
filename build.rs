21de94aecee4c0b657fee4ba0ccb6eae60aee540
Ashley Ruglys <ashley.ruglys@gmail.com>
2023-05-06 18:29:32 +1200

    Updates

--- build.rs
+++ build.rs
@@ -1,9 +1,86 @@
+fn bind_shader(shader: &str) {
+    println!("cargo:rustc-link-lib=static={}", shader);
+    println!("cargo:rerun-if-changed=lib{}.a", shader);
+}
+
 fn main() {
+    let source_file = "callback_runtime.cpp";
+    let object_file = "callback_runtime.o";
+    let archive_file = "libcallback_runtime.a";
+
+    let bindings = bindgen::Builder::default()
+        .header(source_file)
+        .opaque_type("std::.*")
+        .allowlist_function("add_callback_runtime")
+        .allowlist_function("rps[^:]*")
+        .allowlist_type("rps[^:]*")
+        .allowlist_type("Rps.*")
+        .allowlist_type("PFN.*")
+        .allowlist_type("rps::RenderGraphUpdateContext")
+        .allowlist_type("Callbacks")
+        .opaque_type("rps::Arena")
+        .opaque_type(".*Pool.*")
+        // This inherits from ParamDecl and is thus pretty broken.
+        .opaque_type("rps::NodeParamDecl")
+        .rustified_enum("Rps.*")
+        .enable_cxx_namespaces()
+        .clang_args([
+            "-I",
+            "RenderPipelineShaders/include",
+            "-I",
+            "RenderPipelineShaders/src",
+            "-stdlib=libc++",
+            "-x",
+            "c++",
+        ])
+        .impl_debug(true)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+
     println!("cargo:rustc-link-search=RenderPipelineShaders/build/src");
     println!("cargo:rustc-link-search=.");
-    println!("cargo:rustc-link-lib=static=hello_triangle");
-    println!("cargo:rustc-link-lib=static=custom_runtime");
+    println!("cargo:rustc-link-lib=static=callback_runtime");
     println!("cargo:rustc-link-lib=rps_runtime");
     println!("cargo:rustc-link-lib=rps_core");
     println!("cargo:rustc-link-lib=stdc++");
+
+    bind_shader("upscale");
+    bind_shader("rps_multithreading");
+
+    println!("cargo:rerun-if-changed={}", source_file);
+
+    if !std::process::Command::new("clang")
+        .arg("-c")
+        .arg("-o")
+        .arg(object_file)
+        .arg(source_file)
+        .arg("-IRenderPipelineShaders/src")
+        .arg("-IRenderPipelineShaders/include")
+        .output()
+        .expect("could not spawn `clang`")
+        .status
+        .success()
+    {
+        // Panic if the command was not successful.
+        panic!("could not compile object file");
+    }
+
+    if !std::process::Command::new("ar")
+        .arg("rcs")
+        .arg(archive_file)
+        .arg(object_file)
+        .output()
+        .expect("could not spawn `ar`")
+        .status
+        .success()
+    {
+        // Panic if the command was not successful.
+        panic!("could not emit library file");
+    }
 }
